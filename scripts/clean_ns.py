d7009339a64c4b72d965fa731f841534ae9b3746
louchenyao <louchenyao@gmail.com>
2020-08-05 13:47:58 +0000

    minor

--- scripts/clean_ns.py
+++ scripts/clean_ns.py
@@ -8,6 +8,8 @@ def list_ns():
     return p.stdout.decode().splitlines()
 
 def del_ns(ns):
+    # for unknown reasons, the listed name may be "hub (id: 0)"
+    ns = ns.split(" ")[0]
     assert(os.system(f"sudo ip netns del {ns}") == 0)
 
 for ns in list_ns():
